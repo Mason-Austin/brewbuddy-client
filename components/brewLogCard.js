b157536642b7c9f1376c7e6dca98ba36ccd80867
Mason Austin <mason47austin@gmail.com>
2024-02-19 15:32:46 -0600

    finished basic brewLogCard

---
+++ components/brewLogCard.js
@@ -0,0 +1,17 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function BrewLogCard(brewLog) {
+  return (
+    <Card>
+      <Card.Header>{brewLog.date}</Card.Header>
+      <Card.Body>
+        <Card.Text>{brewLog.log}</Card.Text>
+        <Button variant="success">Edit</Button>
+        <Button variant="danger">Delete</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default BrewLogCard;
