1bcf51dc000255f70762b92018b239e1c2d12e17
Mason Austin <mason47austin@gmail.com>
2024-02-19 11:46:42 -0600

    created all api calls that deal with categories

---
+++ API/categoryApi.js
@@ -0,0 +1,43 @@
+import { clientCredentials } from "../utils/client";
+
+const getAllCategories = () => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/categories`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getSingleCategory = (categoryId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/categories/${categoryId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const deleteBrewCategory = (brewCategoryId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/brews/${brewCategoryId}/remove_category`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(resolve)
+    .catch(reject);
+});
+
+const createBrewCategory = (categoryId, brewId) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}brews/${brewId}/add_item_to_order`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(categoryId),
+  })
+    .then((data) => resolve(data))
+    .catch((error) => reject(error));
+});
